8c600915122d0e92b48f62e8d8b27f1f238982af
vladimirbestwork <vladimir.bestwork@gmail.com>
2017-03-13 05:29:55 +0200

    Фикс установки дополнений

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://santehnik.ua/admin/');
+define('HTTP_CATALOG', 'http://santehnik.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://santehnik.ua/admin/');
+define('HTTPS_CATALOG', 'http://santehnik.ua/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/WebServer/OpenServer/domains/santehnik.ua/admin/');
+define('DIR_SYSTEM', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/');
+define('DIR_LANGUAGE', 'D:/WebServer/OpenServer/domains/santehnik.ua/admin/language/');
+define('DIR_TEMPLATE', 'D:/WebServer/OpenServer/domains/santehnik.ua/admin/view/template/');
+define('DIR_CONFIG', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/config/');
+define('DIR_IMAGE', 'D:/WebServer/OpenServer/domains/santehnik.ua/image/');
+define('DIR_CACHE', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/download/');
+define('DIR_LOGS', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/upload/');
+define('DIR_CATALOG', 'D:/WebServer/OpenServer/domains/santehnik.ua/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'santehnik');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://santehnik.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://santehnik.ua/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/WebServer/OpenServer/domains/santehnik.ua/catalog/');
+define('DIR_SYSTEM', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/');
+define('DIR_LANGUAGE', 'D:/WebServer/OpenServer/domains/santehnik.ua/catalog/language/');
+define('DIR_TEMPLATE', 'D:/WebServer/OpenServer/domains/santehnik.ua/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/config/');
+define('DIR_IMAGE', 'D:/WebServer/OpenServer/domains/santehnik.ua/image/');
+define('DIR_CACHE', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/download/');
+define('DIR_LOGS', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/WebServer/OpenServer/domains/santehnik.ua/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'santehnik');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
